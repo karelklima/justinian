ed5e00617f21897483d5d9d4674e223b3dedd2f8
michal-au <michal.au@seznam.cz>
2014-04-18 10:46:43 +0200

    #41 related-acts api #46 pridany filtr

--- backend/lib/sparql-query.js
+++ backend/lib/sparql-query.js
@@ -31,6 +31,10 @@ SparqlQuery.prototype.filterEscapeDoubleQuotes = function(string) {
     return string.replace(/"/g, '\\"');
 };
 
+SparqlQuery.prototype.filterEscapeGreaterThen = function(string) {
+	return string.replace(/(>)/g, '\\>');
+}
+
 SparqlQuery.prototype.renderQuery = function(params) {
     params = params || {};
 
---
+++ modules/lex/api/related-acts.js
@@ -0,0 +1,3 @@
+module.exports = function(routeParams) {
+    return routeParams.SparqlRoute;
+};
