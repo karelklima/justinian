289dc96dafbc5df365238a6db0ce65d8c47910be
Fantomaus <fantomaus@gmail.com>
2014-04-28 23:01:27 +0200

    #59 fix zanoreni direktivy

--- frontend/js/directives.js
+++ frontend/js/directives.js
@@ -7,28 +7,28 @@ appDirectives = angular.module('appDirectives', [
 appDirectives.directive('click', ['UrlService', function (UrlService) {
     return {
         restrict: 'A',
+        replace: true,
         transclude: true,
         scope: {
             module: '@',
             application: '@',
             resource: '@',
             type: '@',
+            query: '@',
             params: '@'
         },
         template: '<a href ng-click="click()" ng-transclude></a>',
-        link: function (scope) {
+        link: function (scope, element, attributes) {
             scope.click = function () {
-                var params;
-                if (angular.isDefined(scope.params)) {
-                    params = angular.fromJson(scope.params);
-                } else {
-                    params = {};
-                }
+                var params = angular.isDefined(scope.params) ? angular.fromJson(scope.params) : {};
                 if (angular.isDefined(scope.resource)) {
                     params['resource'] = scope.resource;
                 }
                 if (angular.isDefined(scope.type)) {
                     params['type'] = scope.type;
+                }
+                if (angular.isDefined(scope.query)) {
+                    params['query'] = scope.query;
                 }
                 UrlService.setUrl(scope.module, scope.application, params);
             }
