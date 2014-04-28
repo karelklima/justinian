7c92098ca6b8b05baded25c0f421ed5e8c10e9ec
Fantomaus <fantomaus@gmail.com>
2014-04-28 02:43:15 +0200

    small fix

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -6,19 +6,19 @@ appControllers = angular.module('appControllers', [
 
 appControllers.controller('RootController', ['$scope', 'ConfigurationService', 'UrlService', 'PageService', function ($scope, ConfigurationService, UrlService, PageService) {
 
-//    // pokud mame url ve tvaru /?type=xxx&resource=yyy najdeme defaultni aplikaci pro zadany typ
-//    if (!UrlService.isParam('module') && !UrlService.isParam('application')
-//        && UrlService.isParam('type') && UrlService.isParam('resource')) {
-//        var path = ConfigurationService.getDefaultModuleApplication(UrlService.getParam('type'));
-//        UrlService.setUrl(path.module, path.application, ['resource']);
-//        return;
-//    }
-//
-//    // pokud neexistuje aplikace, kterou mame zobrazit jdeme na errorpage
-//    if (!ConfigurationService.isModuleApplication(UrlService.getParam('module'), UrlService.getParam('application'))) {
-//        UrlService.setUrlError();
-//        return;
-//    }
+    // pokud mame url ve tvaru /?type=xxx&resource=yyy najdeme defaultni aplikaci pro zadany typ
+    if (!UrlService.isParam('module') && !UrlService.isParam('application')
+        && UrlService.isParam('type') && UrlService.isParam('resource')) {
+        var path = ConfigurationService.getDefaultModuleApplication(UrlService.getParam('type'));
+        UrlService.setUrl(path.module, path.application, ['resource']);
+        return;
+    }
+
+    // pokud neexistuje aplikace, kterou mame zobrazit jdeme na errorpage
+    if (!ConfigurationService.isModuleApplication(UrlService.getParam('module'), UrlService.getParam('application'))) {
+        UrlService.setUrlError();
+        return;
+    }
 
     PageService.setTitle(ConfigurationService.getDefaultTitle());
 
