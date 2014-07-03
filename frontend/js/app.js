5b109f4d8f81d05ae449b777ba546712cf6bc89a
karelklima <karelklima@gmail.com>
2014-07-03 22:40:51 +0200

    progress bar #96

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,43 +1,51 @@
-var mainApp;
+(function() {
 
-mainApp = angular.module('mainApp', [
-    'ngRoute',
-    'appControllers',
-    'appDirectives',
-    'appFilters'
-]);
+    angular.module('mainApp', [
+        'ngRoute',
+        'angular-loading-bar',
+        'ngAnimate',
+        'appControllers',
+        'appDirectives',
+        'appFilters'
+    ])
 
-mainApp.config(function ($routeProvider, $locationProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'partials/root.html',
-        controller: 'RootController',
-        controllerAs: 'root',
-        reloadOnSearch: false
-    });
-    $routeProvider.when('/:module/:application', {
-        templateUrl: 'partials/root.html',
-        controller: 'RootController',
-        controllerAs: 'root',
-        reloadOnSearch: false
-    });
-    // TODO configuration service
-    $routeProvider.otherwise({redirectTo: '/' + configuration.application.home.module + '/' + configuration.application.home.application});
-    $locationProvider.html5Mode(false);
-
-    //If you want to set $locationProvider.hashPrefix, you should to check "click" directive -> line directives.js:33 `attributes.$set('href', XXX`
-    //$locationProvider.hashPrefix('!');
-});
-
-mainApp.config(['$provide', function($provide){
-    $provide.decorator('$rootScope', ['$delegate', function($delegate){
-        Object.defineProperty($delegate.constructor.prototype, '$listen', {
-            value: function(name, listener){
-                var unsubscribe = $delegate.$on(name, listener);
-                this.$on('$destroy', unsubscribe);
-                return unsubscribe;
-            },
-            enumerable: false
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'partials/root.html',
+            controller: 'RootController',
+            controllerAs: 'root',
+            reloadOnSearch: false
         });
-        return $delegate;
+        $routeProvider.when('/:module/:application', {
+            templateUrl: 'partials/root.html',
+            controller: 'RootController',
+            controllerAs: 'root',
+            reloadOnSearch: false
+        });
+        // TODO configuration service
+        $routeProvider.otherwise({redirectTo: '/' + configuration.application.home.module + '/' + configuration.application.home.application});
+        $locationProvider.html5Mode(false);
+
+        //If you want to set $locationProvider.hashPrefix, you should to check "click" directive -> line directives.js:33 `attributes.$set('href', XXX`
+        //$locationProvider.hashPrefix('!');
+    })
+
+    .config(['$provide', function ($provide) {
+        $provide.decorator('$rootScope', ['$delegate', function ($delegate) {
+            Object.defineProperty($delegate.constructor.prototype, '$listen', {
+                value: function (name, listener) {
+                    var unsubscribe = $delegate.$on(name, listener);
+                    this.$on('$destroy', unsubscribe);
+                    return unsubscribe;
+                },
+                enumerable: false
+            });
+            return $delegate;
+        }]);
+    }])
+
+    .config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
+        cfpLoadingBarProvider.includeSpinner = false;
     }]);
-}]);
+
+})();
