9ed66fa6165d1304c48e9ec6717d3c51510c937c
Fantomaus <fantomaus@gmail.com>
2014-07-08 21:22:19 +0200

    uprava api

--- modules/court/api/act-related-decisions.js
+++ modules/court/api/act-related-decisions.js
@@ -1,44 +1,25 @@
 module.exports = function(routeParams) {
-    
+
+    var _ = routeParams.Underscore;
     var route = new routeParams.SparqlRouteJSONLD;
 
-    route.prepareResponse = function(responseString, next) {
+    route.getContext = function() {
+        return {
+            "title" : "http://purl.org/dc/terms/title",
+            "valid" : {
+                "@id" : "http://purl.org/dc/terms/valid",
+                "@type": "http://www.w3.org/2001/XMLSchema#date"
+            }
+        }
+    };
 
-        var decodeUnicode = function(encodedString) {
-            var decodeUnicodeStringRegex = /\\u([\d\w]{4})/gi;
-            return encodedString.replace(decodeUnicodeStringRegex, function (match, grp) {
-                return String.fromCharCode(parseInt(grp, 16));
-            });
-        };
+    route.prepareResponse = function(responseJSONLD, next) {
 
-        var data = JSON.parse(responseString);
-        var table = [];
-
-        data.forEach(function(item) {
-            var row = {};
-
-            var resource = item["@id"];
-            var title = item["http://purl.org/dc/terms/title"][0]["@value"];
-
-            var valid = [];
-            item["http://purl.org/dc/terms/valid"].forEach(function(value) {
-                valid.push(new Date(value["@value"].substring(0, 10)).valueOf());
-            });
-
-            var subject = [];
-            item["http://purl.org/dc/terms/subject"].forEach(function(value) {
-                subject.push(decodeUnicode(value["@value"]));
-            });
-
-            row['resource'] = resource;
-            row['title'] = title;
-            row['valid'] = valid;
-            row['subject'] = subject.join(', ');
-
-            table.push(row);
+        responseJSONLD["@graph"].forEach(function(data) {
+            data["valid-utc"] = _.has(data, "valid") ? (new Date(data["valid"].substring(0, 10))).valueOf() : "";
         });
 
-        next(JSON.stringify(table));
+        next(responseJSONLD);
     };
 
     return route;
