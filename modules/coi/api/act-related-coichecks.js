1c5d9124fd3cc9b44cf0e556185d16e3f55dc3e0
karelklima <karelklima@gmail.com>
2014-07-11 17:39:34 +0200

    souvisejici kontroly COI #91 #92

---
+++ modules/coi/api/act-related-coichecks.js
@@ -0,0 +1,60 @@
+module.exports = function(routeParams) {
+
+    var _ = routeParams.Underscore;
+    var route = new routeParams.SparqlRouteJSONLD;
+
+    route.getContext = function() {
+        return {
+            "location" : "http://schema.org/location",
+            "date" : {
+                "@id" : "http://purl.org/dc/terms/date",
+                "@type": "http://www.w3.org/2001/XMLSchema#date"
+            },
+            "result" : "http://schema.org/result",
+            "object" : "http://schema.org/object"
+        }
+    };
+
+    route.prepareResponse = function(responseJSONLD) {
+
+        var regions =  {
+            "Hlavní město Praha": "PHA",
+            "Středočeský kraj": "STČ",
+            "Jihočeský kraj": "JHČ",
+            "Plzeňský kraj": "PLK",
+            "Karlovarský kraj": "KVK",
+            "Ústecký kraj": "ULK",
+            "Liberecký kraj": "LBK",
+            "Královéhradecký kraj": "HKK",
+            "Pardubický kraj": "PAK",
+            "Kraj Vysočina": "VYS",
+            "Jihomoravský kraj": "JHM",
+            "Olomoucký kraj": "OLK",
+            "Zlínský kraj": "ZLK",
+            "Moravskoslezský kraj": "MSK"
+        };
+
+        responseJSONLD["@graph"].forEach(function(data) {
+            data["location-short"] = _.has(regions, data["location"]) ? regions[data["location"]] : data["location"];
+            data["title"] = data["@id"].substring(data["@id"].lastIndexOf("/check-action/") + 14);
+        });
+
+        return responseJSONLD;
+    };
+
+    route.getModel = function() {
+        return {
+            "@id" : ["string", ""],
+            "location" : ["string", ""],
+            "location-short" : ["string", ""],
+            "date" : ["string", ""],
+            "date-utc" : ["number", undefined],
+            "result" : ["array", []],
+            "object" : ["array", []],
+            "title" : ["string", ""]
+        }
+    };
+
+
+    return route;
+};
---
+++ modules/coi/apps/act-related-coichecks/js/controllers.js
@@ -0,0 +1,39 @@
+(function() {
+    angular.module('appControllers')
+        .controller('ActRelatedCoiChecksController', ['$scope', 'NetworkService', 'UrlService', 'AppService', function ($scope, NetworkService, UrlService, AppService) {
+
+//            $scope.resource = UrlService.getParam('resource');
+
+            $scope.checks = undefined;
+            $scope.property = '-date-utc';
+            $scope.reverse = false;
+
+            $scope.sortBy = function (property) {
+                if ($scope.property === property) {
+                    $scope.reverse = !$scope.reverse;
+                } else {
+                    $scope.property = property;
+                    $scope.reverse = false;
+                }
+            };
+
+            $scope.isLoading = function() {
+                return angular.isUndefined($scope.checks);
+            };
+
+            $scope.isEmpty = function() {
+                return angular.isDefined($scope.checks) && $scope.checks.length === 0;
+            };
+
+            this.update = function () {
+                NetworkService.getData('coi', 'act-related-coichecks', {'resource': $scope.resource})
+                    .then(function (checks) {
+                        $scope.checks = checks["@graph"];
+                    });
+            };
+
+            AppService.init($scope, ['resource'], this.update);
+
+        }]);
+
+})();
