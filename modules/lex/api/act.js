f2ac1bd0e35bf3295c663845aba181faa09f0af7
michal-au <michal.au@seznam.cz>
2014-07-14 16:39:05 +0200

    #101 act API

--- modules/lex/api/act.js
+++ modules/lex/api/act.js
@@ -1,3 +1,47 @@
 module.exports = function(routeParams) {
-    return new routeParams.SparqlRouteJSONLD;
+    var _ = routeParams.Underscore;
+    var route = new routeParams.SparqlRouteJSONLD;
+
+    route.getContext = function() {
+        return {
+        	"title" : {
+        		"@id" : "http://purl.org/dc/terms/title",
+        		"@language" : "cs"
+        	},
+            "issued" : {
+                "@id" : "http://purl.org/dc/terms/issued",
+                "@type" : "http://www.w3.org/2001/XMLSchema#date"
+            },
+            "creator" : "http://purl.org/dc/terms/creator",
+            "identifier" : "http://purl.org/dc/terms/identifier",            
+            "lastVersion" : "http://purl.org/vocab/frbr/core#realization",
+            "lastVersionValid" : {
+                "@id" : "http://purl.org/dc/terms/lastVersionValid",
+                "@type": "http://www.w3.org/2001/XMLSchema#date"
+            }
+        }
+    };
+
+   route.prepareResponse = function(responseJSONLD, next) {
+        responseJSONLD["@graph"].forEach(function(item) {
+            item["issued-utc"] = Number(item["issued-utc"]);
+            item["lastVersionValid-utc"] = Number(item["lastVersionValid-utc"]); 
+        });
+        
+        return responseJSONLD;
+    };
+
+    route.getModel = function() {
+        return {
+            "@id" : ["string", ""],
+            "title" : ["string", ""],
+        	"issued-utc" : ["number", undefined],
+            "creator" : ["string", ""],
+            "identifier" : ["string", ""],
+            "lastVersion" : ["string", ""],
+            "lastVersionValid-utc" : ["number", undefined]
+        }
+    };
+    
+    return route;
 };
