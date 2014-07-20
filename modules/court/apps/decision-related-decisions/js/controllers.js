68f9bcedc45f2183f618996d618874e9c02c5dcb
michal-au <michal.au@seznam.cz>
2014-07-20 21:36:57 +0200

    #121 decision-related-decisions

---
+++ modules/court/apps/decision-related-decisions/js/controllers.js
@@ -0,0 +1,37 @@
+(function() {
+    angular.module('appControllers')
+        .controller('DecisionRelatedDecisionsController', ['$scope', 'NetworkService', 'AppService', function ($scope, NetworkService, AppService) {
+
+            $scope.decisions = undefined;
+            $scope.predicate = '-issuedIso';
+            $scope.reverse = false;
+
+            $scope.sortBy = function (predicate) {
+                if ($scope.predicate === predicate) {
+                    $scope.reverse = !$scope.reverse;
+                } else {
+                    $scope.predicate = predicate;
+                    $scope.reverse = false;
+                }
+            };
+
+            $scope.isLoading = function() {
+                return angular.isUndefined($scope.decisions);
+            };
+
+            $scope.isEmpty = function() {
+                return angular.isDefined($scope.decisions) && $scope.decisions.length === 0;
+            };
+
+            this.update = function () {
+                NetworkService.getData('court', 'decision-related-decisions', {'resource': $scope.resource})
+                    .then(function (decisions) {
+                        $scope.decisions = decisions["@graph"];
+                    });
+            };
+
+            AppService.init($scope, ['resource'], this.update);
+
+        }]);
+
+})();
