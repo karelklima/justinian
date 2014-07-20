d46061ea1d6b074fc8da276725425cf4c4d0ed99
michal-au <michal.au@seznam.cz>
2014-07-20 14:47:17 +0200

    #101 decision-related-acts

---
+++ modules/lex/api/decision-related-acts.js
@@ -0,0 +1,36 @@
+module.exports = function(routeParams) {
+
+    var _ = routeParams.Underscore;
+    var route = new routeParams.SparqlRouteJSONLD;
+
+    route.getContext = function() {
+        return {
+            "relatedActs" : "http://purl.org/lex#concernedSourceOfLaw",
+            "title": "http://purl.org/dc/terms/title"
+        }
+    };
+
+    route.prepareResponse = function(responseJSONLD) {
+
+    	responseJSONLD["@graph"].forEach(function(data) {
+            if(_.isArray(data["title"]))
+            {
+            	data["title"].forEach(function(title) {            
+            		if(_.isObject(title)) data["title"] = title["@value"];
+            	});
+            }
+        });
+
+        return responseJSONLD;
+    };
+
+    route.getModel = function() {
+        return {
+            "@id" : ["string", ""],
+            "relatedActs" : ["array", []],
+            "title" : ["string", ""]
+        }
+    };
+
+    return route;
+};
--- modules/lex/api/related-acts.js
+++
@@ -1,3 +0,0 @@
-module.exports = function(routeParams) {
-    return new routeParams.SparqlRouteJSONLD;
-};
