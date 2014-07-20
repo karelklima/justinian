027fb05068ce6d76f91bfdcee0763358d82e8ed0
karelklima <karelklima@gmail.com>
2014-07-20 13:50:07 +0200

    prefix-replacer - nahrazovani prefixu nezavisle na modulu #119

--- backend/lib/prefix-replacer.js
+++ backend/lib/prefix-replacer.js
@@ -1,7 +1,3 @@
-/**
- * Created by Karel on 22. 4. 2014.
- */
-
 var _ = require('underscore');
 var settings = require('./settings');
 
@@ -12,47 +8,37 @@ exports = module.exports = instance;
 
 function PrefixReplacer()
 {
-    this.prefixes = {};
+    var prefixes = {};
     var setup = settings.getModulesSetup();
-    var modules = _.keys(setup);
-    for (var i = 0; i < modules.length; i++)
-    {
-        this.prefixes[modules[i]] = {};
-        if (!setup[modules[i]].hasOwnProperty(PREFIXES_KEY))
-            continue;
-        var modulePrefixes = _.keys(setup[modules[i]][PREFIXES_KEY]);
-        for (var j = 0; j < modulePrefixes.length; j++)
-        {
-            this.prefixes[modules[i]][modulePrefixes[j]] = setup[modules[i]][PREFIXES_KEY][modulePrefixes[j]];
+    _.each(setup, function(spec, module) {
+        if (_.isObject(spec[PREFIXES_KEY])) {
+            var p = spec[PREFIXES_KEY];
+            if (_.every(_.keys(p), function(key) { return !_.has(prefixes, key) || prefixes[key] == p[key]; })) {
+                _.extend(prefixes, p);
+            } else {
+                throw Error("Invalid modules configuration, prefix collision detected in module " + module);
+            }
         }
-    }
+    });
+    this.prefixes = prefixes;
 }
 
-PrefixReplacer.prototype.validate = function(module)
+PrefixReplacer.prototype.expandString = function(value)
 {
-    if (!_.contains(_.keys(this.prefixes), module) || typeof this.prefixes[module] != "object")
-       throw new Error("Unknown module: " + module);
-};
-
-
-PrefixReplacer.prototype.expandString = function(module, value)
-{
-    this.validate(module);
-    for (var prefix in this.prefixes[module])
+    for (var prefix in this.prefixes)
     {
         if (value.indexOf(prefix) == 0)
-            return this.prefixes[module][prefix] + value.substr(prefix.length);
+            return this.prefixes[prefix] + value.substr(prefix.length);
     }
     return value;
 };
 
-PrefixReplacer.prototype.contractString = function(module, value)
+PrefixReplacer.prototype.contractString = function(value)
 {
-    this.validate(module);
-    for (var prefix in this.prefixes[module])
+    for (var prefix in this.prefixes)
     {
-        if (value.indexOf(this.prefixes[module][prefix]) == 0)
-            return prefix + value.substr(this.prefixes[module][prefix].length);
+        if (value.indexOf(this.prefixes[prefix]) == 0)
+            return prefix + value.substr(this.prefixes[prefix].length);
     }
     return value;
 };
