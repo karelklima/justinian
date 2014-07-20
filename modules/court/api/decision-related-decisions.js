c0b5bccfb3b80362ec014234130a8dfcefd5cb65
michal-au <michal.au@seznam.cz>
2014-07-20 21:20:14 +0200

    #102 decision-related-decisions API

--- modules/court/api/decision-related-decisions.js
+++ modules/court/api/decision-related-decisions.js
@@ -1,3 +1,37 @@
 module.exports = function(routeParams) {
-    return new routeParams.SparqlRouteJSONLD;
+
+    var _ = routeParams.Underscore;
+    var route = new routeParams.SparqlRouteJSONLD;
+
+    route.getContext = function() {
+        return {
+            "subject" : "http://purl.org/dc/terms/subject",
+            "title" : "http://purl.org/dc/terms/title",
+            "issued" : {
+                "@id" : "http://purl.org/dc/terms/issued",
+                "@type": "http://www.w3.org/2001/XMLSchema#date"
+            }
+        }
+    };
+
+    route.prepareResponse = function(responseJSONLD) {
+
+        responseJSONLD["@graph"].forEach(function(data) {
+            data["subject"] = _.isArray(data["subject"]) ? data["subject"].join(', ') : "";
+        });
+
+        return responseJSONLD;
+    };
+
+    route.getModel = function() {
+        return {
+            "@id" : ["string", ""],
+            "title" : ["string", ""],
+            "issuedIso" : ["string", ""],
+            "subject": ["string", ""]
+        }
+    };
+
+    return route;
+
 };
--- modules/lex/api/act-expression.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Karel on 30. 3. 2014.
- */
-
-module.exports = function(routeParams) {
-    return new routeParams.SparqlRouteJSONLD;
-};
