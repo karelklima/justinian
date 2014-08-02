3ca93d0bf046d4bf62a8b5bf7136ae5c1967039b
michal-au <michal.au@seznam.cz>
2014-08-02 15:25:16 +0200

    #130 checkactions API

---
+++ modules/coi/api/checkactions.js
@@ -0,0 +1,88 @@
+module.exports = function(routeParams) {
+
+    var _ = routeParams.Underscore;
+
+    var route = new routeParams.SparqlRouteJSONLD;
+
+    route.prepareParams = function(params) {
+    	var outputParams = {};
+    	outputParams.values = "";
+    	
+    	for(var param in params) {
+    		
+    		// remove incoming doublequotes
+    		params[param] = params[param].replace(/"/g, '');
+    		
+    		// construct the SPARQL query
+    		switch(param) {
+    			case "region" : outputParams.values += "?check s:location/s:addressRegion \"" + params["region"] + "\".\n"; break;
+    			case "state"  : outputParams.values += "?check s:object/ares:stav-zivnosti/skos:prefLabel \"" + params["state"] + "\"@cs.\n"; break;
+    			case "dateGT" : outputParams.values += "?check dcterms:date ?date. \nFILTER ( ?date >= \"" + params["dateGT"] + "\"^^xsd:date)\n"; break;
+    			case "dateLT" : outputParams.values += "?check dcterms:date ?date. \nFILTER ( ?date <= \"" + params["dateLT"] + "\"^^xsd:date)\n"; break;
+    			case "town":
+    				outputParams.values +=
+    					"?check s:location ?location.\n" +
+    					"?location s:addressLocality ?locality.\n" +
+    					"?locality bif:contains '[__enc \"UTF-8\"]\""
+    					+ params["locality"] + "\"'.\n";
+    				break;
+    			case "street":
+    				outputParams.values +=
+    					"?check s:location ?location.\n" +
+    					"?location s:streetAddress ?street.\n" +
+    					"?street bif:contains '[__enc \"UTF-8\"]\""
+    					+ params["street"] + "\"'.\n";
+    				break;
+    			case "zipcode" : outputParams.values += "?check s:location ?location. \n?location s:postalCode \"" + params["zipcode"] + "\".\n"; break;
+    			case "objectName" :
+    				outputParams.values +=
+    					"?check s:object ?object.\n" +
+    					"?object gr:legalName ?objectName.\n" +
+    					"?objectName bif:contains '[__enc \"UTF-8\"]\""
+    					+ params["objectName"] + "\"'.\n";
+    				break;
+    			case "activity" :
+    				outputParams.values +=
+    					"?check s:object ?object.\n" +
+    					"?object regorg:orgActivity ?activity.\n" +
+    					"?activity skos:prefLabel ?label.\n" +
+    					"?label bif:contains '[__enc \"UTF-8\"]\""
+    					+ params["activity"] + "\"'.\n";
+    				break;
+    			case "trade" :
+    				outputParams.values +=
+    					"?check s:object ?object.\n" +
+    					"?object ares:zivnost ?trade.\n" +
+    					"?trade dcterms:title ?tradeTitle.\n" +
+    					"?tradeTitle bif:contains '[__enc \"UTF-8\"]\""
+    					+ params["trade"] + "\"'.\n";
+    				break;
+    			default : outputParams[param] = params[param];
+ 
+           }
+        }
+
+        return outputParams;
+    };
+
+    route.getContext = function() {
+        return {
+        	"title": "http://www.w3.org/2004/02/skos/core#notation",
+            "date" : {
+            	"@id" : "http://purl.org/dc/terms/date",
+            	"@type": "http://www.w3.org/2001/XMLSchema#date"
+        	},
+        }
+    };
+
+    route.getModel = function() {
+        return {
+            "@id" : ["string", ""],
+            "title" : ["string", ""],
+        	"dateIso" : ["string", ""]
+        }
+    };
+
+    return route;
+
+};
