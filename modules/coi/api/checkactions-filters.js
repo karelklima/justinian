2c5520f21e5b24e82e604a50bf026e3cc84fb139
michal-au <michal.au@seznam.cz>
2014-08-02 09:38:19 +0200

    #132 checkactions-filters API

---
+++ modules/coi/api/checkactions-filters.js
@@ -0,0 +1,36 @@
+module.exports = function(routeParams) {
+;
+    var _ = routeParams.Underscore;
+    var route = new routeParams.SparqlRouteJSONLD;
+
+    route.getContext = function() {
+        return {
+            "location" : "http://schema.org/location",
+            "regions" : "http://haveRegions",
+            "minDate" : {
+            	"@id" : "http://haveMinDate",
+            	"@type": "http://www.w3.org/2001/XMLSchema#date"
+            },
+            "maxDate" : {
+            	"@id" : "http://haveMaxDate",
+            	"@type": "http://www.w3.org/2001/XMLSchema#date"
+        	},
+        	"states" : {
+        		"@id" : "http://haveStates",
+        		"@language": "cs"
+        	}
+        }
+    };
+
+    route.getModel = function() {
+        return {
+            "@id" : ["string", ""],
+        	"regions" : ["object", []],
+        	"states" : ["object", []],
+        	"maxDateIso" : ["string", ""],
+        	"minDateIso" : ["string", ""]
+        }
+    };
+
+    return route;
+};
