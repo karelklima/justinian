45f8efa38fe2dc3e1e0677b62d4788dbe3e43376
karelklima <karelklima@gmail.com>
2014-08-03 14:31:31 +0200

    uprava designu vyhledavani #118

--- modules/core/apps/search/js/controllers.js
+++ modules/core/apps/search/js/controllers.js
@@ -1,31 +1,3 @@
-/**
- * Created by Fantomaus on 31. 3. 2014.
- */
-
-/*function LexSearchController($scope, NetworkService, UrlService, UtilService, $rootScope, AppService){
-    $scope.results = [];
-    $scope.searching = false;
-
-    $scope.searchIteration = 0; //many search requests in short time interval -> we need to show only last one.
-    $scope.search = function(){
-        var query = $scope.query;
-        if(!angular.isDefined(query) || query.length == 0)
-            return;
-        $scope.searching = true;
-        $scope.searchIteration++;
-        var currentIteration = $scope.searchIteration;
-
-        NetworkService.getData('core', 'act-search', {'query': query, 'limit':5, 'offset':0})
-            .then(function (data) {
-                if($scope.searchIteration != currentIteration) return;
-                $scope.results = data["@graph"];
-                $scope.searching = false;
-            });
-    }
-
-    AppService.init($scope, ['query'], $scope.search);
-}*/
-
 (function() {
 
     angular.module('appControllers')
@@ -50,11 +22,15 @@
 
                 },
 
-                loading: function(val) {
+                loading: function (val) {
                     $scope.loading = val;
-                }
+                },
 
+                revision: function() {
+                    return $scope.query;
+                }
             };
+
 
 
         }]);
