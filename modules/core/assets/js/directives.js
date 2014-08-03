810c3f8c912216aedb71cca2c8a469cbdf5bf629
karelklima <karelklima@gmail.com>
2014-08-03 14:28:54 +0200

    presun direktiv pro miniaplikace z modulu justinian do core

--- modules/core/assets/js/directives.js
+++ modules/core/assets/js/directives.js
@@ -1,3 +1,95 @@
-/**
- * Created by Karel on 10.3.14.
- */
+(function() {
+
+    angular.module("appDirectives")
+
+        .directive('mainApp', function () {
+            return {
+                restrict: 'A',
+                transclude: true,
+                replace: true,
+                template:
+                    "<div class=\"panel\">\n" +
+                    "  <div class=\"panel-body transclude\"></div>\n" +
+                    "</div>",
+                link: function(scope, element, attrs, controller, linker) {
+                    // Custom transclusion so we can share scope with custom controller
+                    linker(scope, function(clone){
+                        element.find(".transclude").append(clone); // add to DOM
+                    });
+                }
+            }
+        })
+
+        .directive('sidebarApp', function () {
+            return {
+                restrict: 'A',
+                transclude: true,              // It transcludes the contents of the directive into the template
+                replace: true,                // The element containing the directive will be replaced with the template
+                template:
+                    "<div class=\"panel panel-default\">\n" +
+                    "  <div class=\"panel-heading cursor-pointer\" ng-click=\"toggleOpen()\">\n" +
+                    "    <h4 class=\"panel-title\">\n" +
+                    "      <a class=\"sidebar-toggle\">\n" +
+                    "        <span ng-class=\"{'text-muted': isDisabled}\">{{heading}}</span>\n" +
+                    "        <i class=\"pull-right glyphicon\" ng-class=\"{'glyphicon-chevron-down': isOpen, 'glyphicon-chevron-right': !isOpen}\"></i>" +
+                    "      </a>\n" +
+                    "    </h4>\n" +
+                    "  </div>\n" +
+                    "  <div class=\"panel-collapse\" collapse=\"!isOpen\">\n" +
+                    "	  <div class=\"panel-body transclude\"></div>\n" +
+                    "  </div>\n" +
+                    "</div>",
+                link: function(scope, element, attrs, controller, linker) {
+                    // Custom transclusion so we can share scope with custom controller
+                    linker(scope, function(clone){
+                        element.find(".transclude").append(clone); // add to DOM
+                    });
+
+                    if (angular.isDefined(attrs.isOpen)) // if it is defined as an attribute, initiate / overwrite scope
+                        scope.isOpen = attrs.isOpen == "true";
+                    else if (angular.isUndefined(scope.isOpen)) // if it is not defined in custom controller, initiate
+                        scope.isOpen = false;
+
+                    if (angular.isDefined(attrs.isDisabled))
+                        scope.isDisabled = attrs.isDisabled == "true";
+                    else if (angular.isUndefined(scope.isDisabled))
+                        scope.isDisabled = false;
+
+                    if (angular.isDefined(attrs.heading))
+                        scope.heading = attrs.heading;
+                    else if (angular.isUndefined(scope.heading))
+                        scope.heading = "Aplikace";
+
+                    scope.toggleOpen = function() {
+                        if ( !scope.isDisabled ) {
+                            scope.isOpen = !scope.isOpen;
+                        }
+                    };
+                }
+            };
+        })
+
+        .directive('anchorScroll', ['$timeout', '$document', function ($timeout, $document) {
+            return {
+                restrict: 'A',
+                scope: {
+                    offsetTop: '@'
+                },
+                link: function (scope, element, attrs) {
+                    if (angular.isUndefined(scope.offsetTop))
+                        scope.offsetTop = element.offset().top;
+                    scope.$on('anchor-scroll', function (events, params) {
+                        $timeout(function () { // You might need this timeout to be sure its run after DOM render.
+                            var target = element.find(params.target);
+                            if (target.length) {
+                               $document.scrollTo(target[0], scope.offsetTop, 300);
+                            }
+                        }, 0, false);
+                    })
+                }
+            }
+        }]);
+
+
+
+})();
