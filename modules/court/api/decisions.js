83e9a4689d80b9732916c94d866cf1b8d4b31980
michal-au <michal.au@seznam.cz>
2014-08-05 19:36:33 +0200

    #95 decisions api

--- modules/court/api/decisions.js
+++ modules/court/api/decisions.js
@@ -8,31 +8,76 @@ module.exports = function(routeParams) {
     route.getContext = function() {
         return {
             "title" : "http://purl.org/dc/terms/title",
- 
             "issued" : {
                 "@id" : "http://purl.org/dc/terms/issued",
                 "@type" : "http://www.w3.org/2001/XMLSchema#date"
             },
             "subject" : "http://purl.org/dc/terms/subject",
-            "totalCount" : "http://TOTALCOUNT"
+            "creator" : {
+            	"@id" : "http://purl.org/dc/terms/creatorTitle",
+            	"@language": "cs"
+            },
+            "title" : "http://purl.org/dc/terms/title",
+            "identifier" : "http://purl.org/dc/terms/identifier",
+            "fileKind" : "http://purl.org/lex#fileKind",
+            "fileNumber" : "http://purl.org/lex#fileNumber",
+            "senateNumber" : "http://purl.org/lex#senateNumber",
+            "decisionCategory" : "http://www.w3.org/2004/02/skos/core#catLabel",
+            "decisionKind" : "http://www.w3.org/2004/02/skos/core#kindLabel",
+            "hasText" : "http://purl.org/dc/terms/hasText"
         };
     };
-
+    
+    route.prepareParams = function(params) {
+    	var outputParams = {};
+    	outputParams.values = " ";
+    	
+	    for(var param in params) {
+			
+			// remove incoming doublequotes
+			params[param] = params[param].replace(/"/g, '');
+			
+			// construct the SPARQL query
+			switch(param) {
+				case "category" : outputParams.values += 
+					"?dec lex:decisionCategory <http://linked.opendata.cz/resource/legislation/cz/decision-category/supreme-court/"
+					+ params["category"].toLowerCase() + "> .\n";
+					break;
+				case "kind"  : outputParams.values += 
+					"?dec lex:decisionKind <http://linked.opendata.cz/resource/legislation/cz/decision-kind/supreme-court/" 
+					+ params["kind"].toLowerCase() + "> .\n";
+					break;
+				case "creator" : outputParams.values += "?dec dcterms:creator <" + params["creator"] + ">. \n"; break;
+				case "subject" : outputParams.values += "?dec dcterms:subject <http://linked.opendata.cz/resource/legislation/cz/decision-subject/" + params["subject"] + ">. \n"; break;
+				case "fileKind" : outputParams.values += "?file lex:fileKind \"" + params["fileKind"] + "\". \n"; break;
+				case "senateNumber" : outputParams.values += "?file lex:senateNumber \"" + params["senateNumber"] + "\". \n"; break;
+				case "fileNumber" : outputParams.values += "?file lex:fileNumber \"" + params["fileNumber"] + "\"^^xsd:integer . \n"; break;
+				case "dateGT" : outputParams.values += "?dec dcterms:issued ?date. \nFILTER ( ?date >= \"" + params["dateGT"] + "\"^^xsd:date)\n"; break;
+				case "dateLT" : outputParams.values += "?dec dcterms:issued ?date. \nFILTER ( ?date <= \"" + params["dateLT"] + "\"^^xsd:date)\n"; break;
+				case "query":
+					outputParams.values +=
+						"?dec  ^frbr:realizationOf ?expr. \n" +
+						"?expr <http://linked.opendata.cz/ontology/odcs/xmlValue> ?text . \n" +
+						"?text bif:contains '[__enc \"UTF-8\"]\""
+						+ params["query"] + "\"'.\n";
+					break;
+				default : outputParams[param] = params[param];
+	
+	       }
+	    }
+	
+	    return outputParams;
+    };
+    
     route.prepareResponse = function(responseJSONLD) {
-        responseJSONLD["@graph"].forEach(function(data) {
-            data["subject"] = _.isArray(data["subject"]) ? data["subject"].join(', ') : "";
-        });
-
-        for (var i = 1; i < responseJSONLD["@graph"].length; i++)
-        {
-            if (responseJSONLD["@graph"][i]["@id"] == "http://RESULTS")
-            {
-                responseJSONLD["@support"] = [responseJSONLD["@graph"][i]];
-                responseJSONLD["@graph"].splice(i, 1);
-                break;
-            }
-        }
-
+    	responseJSONLD["@graph"].forEach(function(data) {
+    		if (_.isArray(data["subject"])) {
+    			data["subjectTitles"] = [];
+    			data["subject"].forEach(function(subject){
+    				data["subjectTitles"].push( subject.substring(subject.lastIndexOf("/") + 1) );
+    			});
+    		}
+    	});
         return responseJSONLD;
     };
 
@@ -41,7 +86,16 @@ module.exports = function(routeParams) {
             "@id" : ["string", ""],
             "title" : ["string", ""],
         	"issuedIso" : ["string", ""],
-            "subject" : ["string", ""]
+            "subjectTitles" : ["object", []],
+            "creator" : ["string", ""],
+            "title" : ["string", ""],
+            "identifier" : ["string", ""],
+            "fileKind" : ["string", ""],
+            "fileNumber" : ["number", 0],
+            "senateNumber" : ["string", ""],
+            "decisionCategory" : ["string", ""],
+            "decisionKind" : ["string", ""],
+            "hasText" : ["number", 0]
         }
     };
     
