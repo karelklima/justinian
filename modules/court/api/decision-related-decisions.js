64136c6f72a36dc18b058ec0a3d179d242463ae3
michal-au <michal.au@seznam.cz>
2014-08-05 13:01:05 +0200

    #121 decision-related-decisions api: přidaná informace o ne/přítomnosti textu pro vracené judikáty

--- modules/court/api/decision-related-decisions.js
+++ modules/court/api/decision-related-decisions.js
@@ -10,14 +10,23 @@ module.exports = function(routeParams) {
             "issued" : {
                 "@id" : "http://purl.org/dc/terms/issued",
                 "@type": "http://www.w3.org/2001/XMLSchema#date"
-            }
+            },
+        	"hasText" : "http://purl.org/dc/terms/hasText"
         }
     };
 
     route.prepareResponse = function(responseJSONLD) {
 
         responseJSONLD["@graph"].forEach(function(data) {
-            data["subject"] = _.isArray(data["subject"]) ? data["subject"].join(', ') : "";
+            if (_.isArray(data["subject"])) {
+            	data["subjectTitle"] = [];
+            	data["subject"].forEach(function(subject){
+            		data["subjectTitle"].push( subject.substring(subject.lastIndexOf("/")) );
+            	});
+            	
+            }
+        	data["subject"] = _.isArray(data["subject"]) ? data["subject"].join(', ') : "";
+            
         });
 
         return responseJSONLD;
@@ -28,7 +37,8 @@ module.exports = function(routeParams) {
             "@id" : ["string", ""],
             "title" : ["string", ""],
             "issuedIso" : ["string", ""],
-            "subject": ["string", ""]
+            "subjectTitle": ["object", []],
+            "hasText" : ["number", 0]
         }
     };
 
