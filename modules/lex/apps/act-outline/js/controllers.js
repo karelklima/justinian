287898bd9bc6d53f69567127532962ccd8e799e2
karelklima <karelklima@gmail.com>
2014-08-07 18:51:13 +0200

    fix obsahu zakona #146

--- modules/lex/apps/act-outline/js/controllers.js
+++ modules/lex/apps/act-outline/js/controllers.js
@@ -20,34 +20,35 @@
             };
 
             this.update = function () {
-                AppService.getData($scope, 'lex', 'act-text', {'resource': $scope.resource})
-                    .then(function (actText) {
-                        if (actText["@graph"].length > 0)
-                        {
-                            var toggle = angular.element('<span ng-click="toggleSection($event)" class="glyphicon glyphicon-plus toggle-button"></span>');
-                            var link = angular.element('<a click></a>');
-                            var doc = angular.element("<div>" + actText["@graph"][0]["htmlValue"] + "</div>");
-                            var content = doc.find("section[class='obsah']");
-                            content.find("section[class='paragraf'], section[class='clanek'], section[class='preambule']").children(':not(header)').remove();
-                            content.find("section[class='cast'], section[class='hlava'], section[class='dil'], section[class='oddil'], section[class='pododdil']").prepend(toggle.clone());
-                            content.find("section").children("header").each(function(index, element) {
-                                var self = angular.element(element);
-                                var text = self.children("h1:first").html();
-                                if (self.children("h1").length > 1)
-                                    text = text + "<span> - </span>" + self.children("h1:last").html();
-                                var cloned = link.clone();
-                                cloned.html(text);
-                                cloned.attr("section", self.closest('section').attr('resource'));
-                                self.prepend(cloned);
-                                self.children("h1").remove();
-                            });
-                            content.find("section section, h2").hide();
-                            $scope.actOutline = content.html();
-                        }
-                    });
+                if (angular.isDefined($scope.version)) {
+                    AppService.getData($scope, 'lex', 'act-text', {'resource': $scope.version})
+                        .then(function (actText) {
+                            if (actText["@graph"].length > 0) {
+                                var toggle = angular.element('<span ng-click="toggleSection($event)" class="glyphicon glyphicon-plus toggle-button"></span>');
+                                var link = angular.element('<a click></a>');
+                                var doc = angular.element("<div>" + actText["@graph"][0]["htmlValue"] + "</div>");
+                                var content = doc.find("section[class='obsah']");
+                                content.find("section[class='paragraf'], section[class='clanek'], section[class='preambule']").children(':not(header)').remove();
+                                content.find("section[class='cast'], section[class='hlava'], section[class='dil'], section[class='oddil'], section[class='pododdil']").prepend(toggle.clone());
+                                content.find("section").children("header").each(function (index, element) {
+                                    var self = angular.element(element);
+                                    var text = self.children("h1:first").html();
+                                    if (self.children("h1").length > 1)
+                                        text = text + "<span> - </span>" + self.children("h1:last").html();
+                                    var cloned = link.clone();
+                                    cloned.html(text);
+                                    cloned.attr("section", self.closest('section').attr('resource'));
+                                    self.prepend(cloned);
+                                    self.children("h1").remove();
+                                });
+                                content.find("section section, h2").hide();
+                                $scope.actOutline = content.html();
+                            }
+                        });
+                }
             };
 
-            AppService.init($scope, ['resource'], this.update);
+            AppService.init($scope, ['resource', 'version'], this.update);
 
         }]);
 
