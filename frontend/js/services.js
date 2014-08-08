17e3de59453b15498d8118dbee4070161f3deab7
Fantomaus <fantomaus@gmail.com>
2014-08-08 22:46:07 +0200

    #110 nebude

--- frontend/js/services.js
+++ frontend/js/services.js
@@ -65,7 +65,7 @@ appServices.service('ConfigurationService', ['UtilService', 'PageService', '$fil
         var result = [];
         angular.forEach(_modules, function (mods, modName) {
             angular.forEach(mods.apps, function (opts, appName) {
-                if (angular.isDefined(opts.datatypes) && opts.datatypes.indexOf(type) !== -1 // FIXME po zmene pole na polozku
+                if (angular.isDefined(opts.datatypes) && opts.datatypes.indexOf(type) !== -1
                         && angular.isDefined(opts.views) && opts.views.indexOf("main") !== -1) {
                     var priority = angular.isDefined(opts.priority) ? opts.priority : 0;
                     result.push({module: modName, application: appName, priority: priority});
