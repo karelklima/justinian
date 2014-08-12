4bea141c5c386431ed8013ce7facc63698448d7c
karelklima <karelklima@gmail.com>
2014-08-12 11:20:20 +0200

    odebrani nepouzitych miniaplikaci z modulu justinian

--- modules/justinian/apps/sidebar-test/js/controllers.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    angular.module('appControllers')
-        .controller('JustinianSidebarTestController', ['$scope', 'NetworkService', 'AppService', function ($scope, NetworkService, AppService) {
-
-            $scope.message = "Funguje to!";
-
-        }]);
-})();
