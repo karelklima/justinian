215f944c12a5524f8dfe8214a02b24f4d9ece27c
karelklima <karelklima@gmail.com>
2014-08-14 23:54:38 +0200

    API pro detail sekce predpisu #156

--- modules/lex/api/act-section-detail.js
+++ modules/lex/api/act-section-detail.js
@@ -6,72 +6,78 @@ module.exports = function(routeParams) {
         return {
             "type" : "http://type",
             "act" : "http://act",
-            "ancestorsRaw" : "http://hasAncestor",
-            "realizations" : "http://hasRealization",
+            "ancestors" : "http://hasAncestor",
+            "realization" : "http://hasRealization",
             "identifier" : "http://purl.org/dc/terms/identifier",
-            "expressionsRaw" : "http://sectionExpression",
+            "expression" : "http://sectionExpression",
             "valid" : {
                 "@id" : "http://purl.org/dc/terms/valid",
                 "@type": "http://www.w3.org/2001/XMLSchema#date"
             }
         }
     };
-    
-   route.prepareResponse = function(responseJSONLD, next) {
-	   
- 	    // copy all the helping data for easier access (their URI as their key): 
-	   	responseJSONLD["@graph"].forEach(function(data) {
-			if ( _.isUndefined(data["ancestorsRaw"]) ) {
-				responseJSONLD["@graph"][data["@id"]] = data;
-			}
-		});
 
-	   	// sorting function for ordering all the expressions of a section according to its validity date:
-		var sortRealizations = function (a,b) {
-			if ( !_.isUndefined(responseJSONLD["@graph"][a]["validIso"]) && !_.isUndefined(responseJSONLD["@graph"][b]["validIso"]) ) {
-				return (responseJSONLD["@graph"][a]["validIso"] > responseJSONLD["@graph"][b]["validIso"]) ? -1 :
-						(responseJSONLD["@graph"][a]["validIso"] < responseJSONLD["@graph"][b]["validIso"]) ? 1 : 0;
-			}
-		}
-			
-	   	responseJSONLD["@graph"].forEach(function(data) {
-	   		if (! _.isUndefined(data["ancestorsRaw"])) {
-	   			
-	   			// the shortest ancestor (URI-wise) is the most general one:
-	   			data["ancestorsRaw"].sort();
-	   			
-	   			// for each ancestor select its last expression
-	   			data["ancestors"] = [];
-	   			data["ancestorsRaw"].forEach(function(ancestor){
-   					if(!_.isUndefined(responseJSONLD["@graph"][ancestor]["realizations"])) {
-   						var realizations = responseJSONLD["@graph"][ancestor]["realizations"];					
-   						realizations.sort(sortRealizations);
-   						var lastExprUri = realizations[0];
-   						
-   						var newAnc = {
-   							"uri" : responseJSONLD["@graph"][ancestor]["@id"],
-   							"identifier" : responseJSONLD["@graph"][ancestor]["identifier"][0],
-   							"expression" : lastExprUri,
-   							"type" : responseJSONLD["@graph"][ancestor]["type"][0]
-   							//"valid" : responseJSONLD["@graph"][lastExprUri]["validIso"][0]
-   						};
-   						
-   						data["ancestors"].push( newAnc );   						
-   					}
-	   			})
-	   	
-	   			// select the last expression of the given section
-	   			data["expressionsRaw"].sort(sortRealizations);
-	   			data["expression"] = data["expressionsRaw"][0];
-	   		}
-	   	})
-	   	
-	   	// clear the "@graph" space:
-		for (var i = responseJSONLD["@graph"].length - 1; i >= 0; i--) {
-	 	   if ( _.isUndefined(responseJSONLD["@graph"][i]["ancestorsRaw"]))
-	 		   responseJSONLD["@graph"].splice(i, 1);
-	 	}
-	 	
+    route.getReconstructComplexObjects = function() {
+        return true;
+    };
+    
+   route.prepareResponse = function(responseJSONLD, requestParams) {
+
+        if (responseJSONLD["@graph"].length > 0) {
+
+            // sorting function for ordering all the expressions of a section according to its validity date:
+            var sortByDate = function (a,b) {
+                if ( !_.isUndefined(a["validIso"]) && !_.isUndefined(b["validIso"]) ) {
+                    return (a["validIso"] > b["validIso"]) ? -1 :
+                        (a["validIso"] < b["validIso"]) ? 1 : 0;
+                }
+            };
+
+            var sortById = function(a, b) {
+                return a["@id"] > b["@id"];
+            };
+
+            var path = [];
+
+            var data = responseJSONLD["@graph"][0];
+            if (_.isArray(data["ancestors"])) {
+                data["ancestors"].sort(sortById);
+                _.each(data["ancestors"], function(ancestor) {
+                    path.push([ancestor["type"], ancestor["identifier"]]);
+                    if (_.isArray(ancestor["realization"])) {
+                        ancestor["realization"].sort(sortByDate);
+                        ancestor["realization"] = ancestor["realization"][0]["@id"];
+                    }
+                });
+            }
+            if (_.isArray(data["expression"])) {
+                data["expression"].sort(sortByDate);
+                data["validIso"] = data["expression"][0]["validIso"];
+                data["expression"] = data["expression"][0]["@id"];
+            }
+
+            path.push([data["type"], data["identifier"]]);
+
+            var formatSection = function(section) {
+                switch (section[0][0]) {
+                    case "http://purl.org/lex/cz#Paragraf":
+                        return "§ " + section[1][0];
+                    case "http://purl.org/lex/cz#Clanek":
+                        return "Čl. " + section[1][0];
+                    case "http://purl.org/lex/cz#Odstavec":
+                        return "odst. " + section[1][0];
+                    case "http://purl.org/lex/cz#Pismeno":
+                        return "písm. " + section[1][0] + ")";
+                    case "http://purl.org/lex/cz#Bod":
+                        return "bod " + section[1][0] + ".";
+                    default:
+                        return "X";
+                }
+            };
+
+            data["title"] = _.map(path, formatSection).join(" ");
+        }
+
         return responseJSONLD;
     };
 
@@ -81,15 +87,25 @@ module.exports = function(routeParams) {
             "type" : ["string", ""],
             "act" : ["string", ""],
             "expression" : ["string", ""],
-            "ancestors" : ["object", []]
+            "validIso" : ["string", ""],
+            "identifier": ["string", ""],
+            "title": ["string", ""],
+            "ancestors" : [{
+                "@id" : ["string", ""],
+                "type" : ["string", ""],
+                "identifier": ["string", ""],
+                "realization": ["string", ""]
+            }, []]
         }
     };
 
     route.getPrefixedProperties = function() {
         return [
             "@id",
-            "ancestors",
-            "expression"
+            "act",
+            "type",
+            "expression",
+            "realization"
         ];
     };
     
