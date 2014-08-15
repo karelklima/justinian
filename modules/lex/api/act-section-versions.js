7b9a2cff7ef72952ced04b27019f06b4532db83c
michal-au <michal.au@seznam.cz>
2014-08-15 16:36:20 +0200

    #165 act-section-versions api

---
+++ modules/lex/api/act-section-versions.js
@@ -0,0 +1,54 @@
+module.exports = function(routeParams) {
+
+    var _ = routeParams.Underscore;
+    var route = new routeParams.SparqlRouteJSONLD;
+
+    route.getContext = function() {
+        return {
+            "valid" : {
+                "@id" : "http://purl.org/dc/terms/valid",
+                "@type" : "http://www.w3.org/2001/XMLSchema#date"
+            },
+            "version" : "http://purl.org/dc/terms/partOf",
+            "identifier" : "http://purl.org/dc/terms/identifier",
+            "amendment" : "http://purl.org/dc/terms/amendment"
+        }
+    };
+
+    route.getReconstructComplexObjects = function() {
+        return true;
+    };
+    
+	
+    route.prepareResponse = function(responseJSONLD, next) {
+	
+    	if (responseJSONLD["@graph"].length > 0) {
+
+    		function sortByProperty(array, propertyName) {
+    			return array.sort(function (a, b) {
+     	            return (a[propertyName] > b[propertyName]) ? -1 :
+     	                    (a[propertyName] < b[propertyName]) ? 1 : 0;
+    			});
+    		}
+
+    		sortByProperty(responseJSONLD["@graph"], "validIso");
+
+		}
+		
+        return responseJSONLD;
+	};
+
+	route.getModel = function() {
+       return {
+           "@id" : ["string", ""],
+           "version": ["object", ""],
+           "validIso" : ["string", ""],
+           "amendment" : [{
+        	   "@id" : ["string", ""],
+        	   "identifier" : ["object", ""]
+           }]
+       }
+	};
+    
+    return route;
+};
