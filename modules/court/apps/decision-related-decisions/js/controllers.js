97186af22b982b3712c6d66b8336b34c74e936ce
Fantomaus <fantomaus@gmail.com>
2014-08-16 16:57:23 +0200

    #121 oprava app-bar a vytvoreni aplikace s mainem dle konvence

--- modules/court/apps/decision-related-decisions/js/controllers.js
+++ modules/court/apps/decision-related-decisions/js/controllers.js
@@ -5,6 +5,27 @@
             $scope.decisions = undefined;
             $scope.predicate = '-issuedIso';
             $scope.reverse = false;
+            $scope.limit = 2;
+
+            $scope.decisionIdentifier = undefined;
+
+            $scope.increase = function() {
+                $scope.datasource.revision = $scope.datasource.revision + 1;
+            };
+
+            $scope.datasource = {
+                get: function(offset, limit, callback) {
+
+                    var params = {'limit': limit, 'offset': offset, 'resource': $scope.resource};
+                    AppService.getData($scope, 'court', 'decision-related-decisions', params)
+                        .then(function (decisions) {
+                            callback(angular.isArray(decisions["@graph"]) ? decisions["@graph"] : []);
+                        }, function(error) {
+                            callback([]);
+                        });
+                },
+                revision: 0
+            };
 
             $scope.sortBy = function (predicate) {
                 if ($scope.predicate === predicate) {
@@ -15,19 +36,26 @@
                 }
             };
 
-            $scope.isLoading = function() {
-                return angular.isUndefined($scope.decisions);
-            };
-
             $scope.isEmpty = function() {
                 return angular.isDefined($scope.decisions) && $scope.decisions.length === 0;
             };
 
+            $scope.isShowMore = function() {
+                return angular.isDefined($scope.decisions) && $scope.decisions.length > $scope.limit;
+            };
+
             this.update = function () {
-                NetworkService.getData('court', 'decision-related-decisions', {'resource': $scope.resource})
-                    .then(function (decisions) {
-                        $scope.decisions = decisions["@graph"];
-                    });
+                $scope.increase();
+
+                if (AppService.isMainApplication("court", "decision-related-decisions")) {
+                    AppService.getData($scope, 'court', 'decision-detail', {'resource': $scope.resource})
+                        .then(function(decisionDetail) {
+                            if (angular.isArray(decisionDetail["@graph"])) {
+                                $scope.decisionIdentifier = decisionDetail["@graph"][0]["identifier"];
+                                AppService.setTitle("Kontroly ČOI k předpisu č. " + $scope.decisionIdentifier);
+                            }
+                        });
+                }
             };
 
             AppService.init($scope, ['resource'], this.update);
