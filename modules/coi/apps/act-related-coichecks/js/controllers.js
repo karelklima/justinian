8c9da4dc72a3dea9592479e416d16d9642965464
Fantomaus <fantomaus@gmail.com>
2014-08-16 15:35:25 +0200

    #92 uprava a main apliakce souvisejich coi

--- modules/coi/apps/act-related-coichecks/js/controllers.js
+++ modules/coi/apps/act-related-coichecks/js/controllers.js
@@ -5,11 +5,27 @@
             $scope.checks = undefined;
             $scope.predicate = '-dateIso';
             $scope.reverse = false;
+            $scope.limit = 9;
 
-            // TODO remove me
-            /*$scope.page = 1;
-            $scope.limit = 5;
-            $scope.size = 5;*/
+            $scope.actIdentifier = undefined;
+
+            $scope.increase = function() {
+                $scope.datasource.revision = $scope.datasource.revision + 1;
+            };
+
+            $scope.datasource = {
+                get: function(offset, limit, callback) {
+
+                    var params = {'limit': limit, 'offset': offset, 'resource': $scope.resource};
+                    AppService.getData($scope, 'coi', 'act-related-coichecks', params)
+                        .then(function (checks) {
+                            callback(angular.isArray(checks["@graph"]) ? checks["@graph"] : []);
+                        }, function(error) {
+                            callback([]);
+                        });
+                },
+                revision: 0
+            };
 
             $scope.sortBy = function (predicate) {
                 if ($scope.predicate === predicate) {
@@ -24,11 +40,22 @@
                 return angular.isDefined($scope.checks) && $scope.checks.length === 0;
             };
 
-            this.update = function () {
-                AppService.getData($scope, 'coi', 'act-related-coichecks', {'resource': $scope.resource})
-                    .then(function (checks) {
-                        $scope.checks = checks["@graph"];
-                    });
+            $scope.isShowMore = function() {
+                return angular.isDefined($scope.checks) && $scope.checks.length > $scope.limit;
+            };
+
+            this.update = function (changes) {
+                $scope.increase();
+
+                if (AppService.isMainApplication("coi", "act-related-coichecks")) {
+                    AppService.getData($scope, 'lex', 'act-detail', {'resource': $scope.resource})
+                        .then(function(actDetail) {
+                            if (angular.isArray(actDetail["@graph"])) {
+                                $scope.actIdentifier = actDetail["@graph"][0]["identifier"];
+                                AppService.setTitle("Kontroly ČOI k předpisu č. " + $scope.actIdentifier);
+                            }
+                        });
+                }
             };
 
             AppService.init($scope, ['resource'], this.update);
