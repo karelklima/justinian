566457d8c41b5039b988d1d7f2edaf38abdb3971
Fantomaus <fantomaus@gmail.com>
2014-08-17 00:18:16 +0200

    #41 oprava controlleru + sidebar  pridani main

--- modules/lex/apps/decision-related-acts/js/controllers.js
+++ modules/lex/apps/decision-related-acts/js/controllers.js
@@ -3,11 +3,15 @@
         .controller('DecisionRelatedActsController', ['$scope', 'NetworkService', 'AppService', function ($scope, NetworkService, AppService) {
 
             $scope.acts = undefined;
+            $scope.predicate = '-issuedIso';
             $scope.reverse = false;
-
-            $scope.page = 1;
             $scope.limit = 2;
-            $scope.size = 5;
+
+            $scope.decisionIdentifier = undefined;
+
+            $scope.increase = function() {
+                $scope.datasource.revision = $scope.datasource.revision + 1;
+            };
 
             $scope.sortBy = function (predicate) {
                 if ($scope.predicate === predicate) {
@@ -18,19 +22,40 @@
                 }
             };
 
+            $scope.datasource = {
+                get: function(offset, limit, callback) {
+
+                    var params = {'limit': limit, 'offset': offset, 'resource': $scope.resource};
+                    AppService.getData($scope, 'lex', 'decision-related-acts', params)
+                        .then(function (acts) {
+                            callback(angular.isArray(acts["@graph"]) ? acts["@graph"] : []);
+                        }, function(error) {
+                            callback([]);
+                        });
+                },
+                revision: 0
+            };
+
             $scope.isEmpty = function() {
                 return angular.isDefined($scope.acts) && $scope.acts.length === 0;
             };
 
-            $scope.isShowPagination = function() {
+            $scope.isShowMore = function() {
                 return angular.isDefined($scope.acts) && $scope.acts.length > $scope.limit;
             };
 
             this.update = function () {
-                AppService.getData($scope, 'lex', 'decision-related-acts', {'resource': $scope.resource})
-                    .then(function (acts) {
-                        $scope.acts = acts["@graph"];
-                    });
+                $scope.increase();
+
+                if (AppService.isMainApplication("lex", "decision-related-acts")) {
+                    AppService.getData($scope, 'court', 'decision-detail', {'resource': $scope.resource})
+                        .then(function(decisionDetail) {
+                            if (angular.isArray(decisionDetail["@graph"])) {
+                                $scope.decisionIdentifier = decisionDetail["@graph"][0]["title"];
+                                AppService.setTitle("Zákony k judikátu č. " + $scope.decisionIdentifier);
+                            }
+                        });
+                }
             };
 
             AppService.init($scope, ['resource'], this.update);
