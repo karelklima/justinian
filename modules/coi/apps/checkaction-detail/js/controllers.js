c044f2dbe0acfc4c1cbfc8b8d24f4072f955da67
Fantomaus <fantomaus@gmail.com>
2014-08-18 01:43:33 +0200

    #147 detail kontroly s google maps

--- modules/coi/apps/checkaction-detail/js/controllers.js
+++ modules/coi/apps/checkaction-detail/js/controllers.js
@@ -4,11 +4,34 @@
 
             $scope.data = undefined;
 
+            $scope.isEmpty = function () {
+                return !$scope.isLoading && angular.isDefined($scope.data) && $scope.data.length === 0;
+            };
+
+            $scope.isLocationAvailable = function(){
+                if(angular.isUndefined($scope.data)) return false;
+                return angular.isDefined($scope.data["street"]) && angular.isDefined($scope.data["locality"]);
+            };
+
             this.update = function() {
 
                 AppService.getData($scope, 'coi', 'checkaction-detail', {'resource': $scope.resource})
                     .then(function(data) {
                         $scope.data = data["@graph"][0];
+                        if($scope.isLocationAvailable()){
+
+                            var locationParam = $scope.data["street"]+", "+
+                                (angular.isDefined($scope.data["zipcode"])?($scope.data["zipcode"]+" "):"");
+
+                            locationParam = locationParam + $scope.data["locality"].replace(/[0-9]+$/, "");
+
+                            locationParam = locationParam.replace(/ /g,"+");
+
+                            var google_maps_url = "https://www.google.cz/maps/embed/v1/search?key=AIzaSyBI6JFokjlnevxRI3FXFwdIwTWXXfw4OPs&q="+
+                                locationParam;
+                            console.log(google_maps_url);
+                            document.getElementById("google_maps_window").src = google_maps_url;
+                        }
                     });
             };
 
