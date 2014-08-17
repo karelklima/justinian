9e671951a3c88faca19461d5f0a03a4227721548
karelklima <karelklima@gmail.com>
2014-08-18 00:04:59 +0200

    fix getDefaultModuleApplication

--- frontend/js/services.js
+++ frontend/js/services.js
@@ -78,7 +78,7 @@ appServices.service('ConfigurationService', ['UtilService', 'PageService', '$fil
         });
         //$log.debug("ConfigurationService.getDefaultModuleApplication: " +  angular.toJson(result));
         var path = $filter('orderBy')(result, 'priority').pop();
-        if (angular.isDefined(path.priority))
+        if (angular.isDefined(path))
             delete path.priority;
         $log.debug("ConfigurationService.getDefaultModuleApplication: " + type + " - " +  angular.toJson(path));
         return path;
