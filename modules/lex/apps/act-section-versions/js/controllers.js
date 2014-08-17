7121ff96e309eae1b933d46888ee17e74b3cfc1f
Fantomaus <fantomaus@gmail.com>
2014-08-17 15:50:37 +0200

    #165 alfa verze historie

---
+++ modules/lex/apps/act-section-versions/js/controllers.js
@@ -0,0 +1,58 @@
+(function() {
+    angular.module('appControllers')
+        .controller('LexSectionVersionsController', ['$scope', '$log', 'NetworkService', 'AppService','UrlService','PageService', function ($scope, $log, NetworkService, AppService, UrlService, PageService) {
+
+            $scope.versions = undefined;
+            $scope.predicate = '-number';
+            $scope.reverse = false;
+
+            $scope.sortBy = function (predicate) {
+                if ($scope.predicate === predicate) {
+                    $scope.reverse = !$scope.reverse;
+                } else {
+                    $scope.predicate = predicate;
+                    $scope.reverse = false;
+                }
+            };
+
+            $scope.limit = 10;
+            $scope.toggleLimit = function() {
+                $scope.limit = ($scope.limit == 10) ? 100 : 10;
+            };
+
+            $scope.isEmpty = function() {
+                return angular.isDefined($scope.versions) && $scope.versions.length === 0;
+            };
+
+            $scope.getVersionValidIso = function(versionId) {
+                var valid = undefined;
+                angular.forEach($scope.versions, function(version) {
+                    if (version['@id'] == versionId)
+                        valid = version['validIso'];
+                });
+                return valid;
+            };
+
+            this.update = function (changes) {
+                if(angular.isDefined(changes['resource'])){
+                    AppService.getData($scope, 'lex', 'act-section-versions', {'resource': $scope.resource, 'limit': 100})
+                        .then(function (data) {
+                            var versions = data["@graph"];
+                            versions.sort(function(v1, v2) {
+                                return (new Date(v1["validIso"])) - (new Date(v2["validIso"]));
+                            });
+                            var counter = 0;
+                            angular.forEach(versions, function(version) {
+                                version["number"] = counter;
+                                counter++;
+                            });
+                            $scope.versions = data["@graph"];
+                        });
+                }
+            };
+
+            AppService.init($scope, ['resource','version','compare'], this.update);
+
+        }]);
+
+})();
