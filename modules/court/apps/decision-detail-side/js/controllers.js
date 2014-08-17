a5ce74eb71ae495b6e27ed8ef6a868840a453e21
Yan Zaitsev <yan.zaitsev@zentity.com>
2014-08-17 02:10:41 +0200

    Informace o judikátu #154

---
+++ modules/court/apps/decision-detail-side/js/controllers.js
@@ -0,0 +1,33 @@
+(function() {
+    angular.module('appControllers')
+        .controller('CourtDecisionDetailSidebarController', ['$scope', '$sce', 'NetworkService', 'AppService', function ($scope, $sce, NetworkService, AppService) {
+
+            $scope.decisionDetail = undefined;
+
+            $scope.isEmpty = function() {
+                return angular.isDefined($scope.decisionDetail) && $scope.decisionDetail.length == 0;
+            };
+
+            $scope.isLoading = function() {
+                return angular.isUndefined($scope.decisionDetail);
+            };
+
+            this.update = function () {
+                $scope.decisionDetail = undefined;
+                AppService.getData($scope, 'court', 'decision-detail', {'resource': $scope.resource})
+                    .then(function (decisionDetail) {
+                        if(angular.isDefined(decisionDetail['@graph']) && decisionDetail['@graph'].length>0){
+                            $scope.decisionDetail = decisionDetail['@graph'][0];
+                        } else {
+                           $scope.decisionDetail = {};
+                        }
+                    }, function fail(){
+                           $scope.decisionDetail = {};
+                    });
+            };
+
+            AppService.init($scope, ['resource'], this.update);
+
+        }]);
+
+})();
