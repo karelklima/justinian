fe03d33c1efce74ce0d1b279d7381507c02a6445
michal-au <michal.au@seznam.cz>
2014-08-18 11:51:51 +0200

    #172 concept-detail api

---
+++ modules/concept/api/concept-detail.js
@@ -0,0 +1,71 @@
+module.exports = function(routeParams) {
+
+    var _ = routeParams.Underscore;
+    var route = new routeParams.SparqlRouteJSONLD;
+
+    route.getContext = function() {
+        return {
+            "act" : "http://hasAct",
+            "actIdRaw" : "http://actId",
+            "actTitle" : { 
+            	"@id" : "http://actTitle",
+            	"@language" : "cs"
+            },
+            "expression" : "http://expression",
+            "definitions" : "http://hasDefinition",
+            "label" : "http://hasLabel",
+            "generalConcepts" : "http://hasGeneralConcepts",
+            "specificConcepts" : "http://hasSpecificConcepts",
+            "textForms" : "http://appearsAs",
+        }
+    };
+
+    route.getReconstructComplexObjects = function() {
+        return true;
+    };
+    
+    /*route.prepareResponse = function(responseJSONLD) {
+        responseJSONLD["@graph"][0]["acts"].forEach( function(act) {
+        	if ( _.isArray(act["actIdRaw"]) ) {
+        		act["actId"] = act["actIdRaw"][0];
+        		act["actIdRaw"].forEach( function(id) {
+        			if ( id.indexOf("Sb.") > -1 ) act["actId"] = id;
+        		});
+        	}
+        });
+
+        return responseJSONLD;
+    };*/
+    
+    route.getModel = function() {
+        return {
+        	"definitions" : ["object", []],
+        	"textForms" : ["object", []],
+        	"label" : ["string", ""],
+        	"generalConcepts" : [{
+        		"@id" : ["string", ""],
+        		"label" : ["string", ""]
+        	}, []],
+        	"specificConcepts" : [{
+        		"@id" : ["string", ""],
+        		"label" : ["string", ""]
+        	}, []],
+        	"act" : [{
+                "@id" : ["string", ""],
+                "actId" : ["string", ""],
+                "actTitle" : ["string", ""],
+                "expression" : ["string", ""],
+            }, []]
+            
+        }
+    };
+
+    route.getPrefixedProperties = function() {
+        return [
+            "@id",
+            "expression"
+        ];
+    };
+    
+    return route;
+};
