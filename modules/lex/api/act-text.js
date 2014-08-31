0f8b9b13ae7c9326320f04b3c130a56d311168e1
karelklima <karelklima@gmail.com>
2014-08-31 14:40:27 +0200

    staticka cache textu predpisu

--- modules/lex/api/act-text.js
+++ modules/lex/api/act-text.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 module.exports = function(routeParams) {
     // return new routeParams.SparqlRoute;
 
@@ -5,6 +7,56 @@ module.exports = function(routeParams) {
     var http = routeParams.HTTP;
     var Q = routeParams.Q;
     var route = new routeParams.SparqlRouteJSONLD;
+
+    route.get = function(req, res) {
+
+        var params = this.prepareParams(req.query);
+
+        var expressionRegex = new RegExp("\/(([0-9]{1,})-([0-9]{1,}))\/.*\/(([0-9]{1,})-([0-9]{1,}))\/");
+
+
+        if (_.isString(params.resource))
+        {
+            var matches = params.resource.match(expressionRegex);
+
+            var padZero = function(string) {
+                return "0000".substring(string.length) + string;
+            };
+
+            var cacheFile = __dirname + "/act-text-cache/pr" + padZero(matches[2]) + '-' + padZero(matches[3]) + '_' + padZero(matches[5]) + '-' + padZero(matches[6]) + '.xml';
+
+            if (fs.existsSync(cacheFile))
+            {
+                var content = fs.readFileSync(cacheFile, "utf8");
+
+                var output = {
+                    "@graph":[{
+                        "@id" : params.resource,
+                        "htmlValue" : content
+                    }]
+                };
+
+                res.write(JSON.stringify(output, null, "  "));
+                res.end();
+                return;
+
+                 var g = 1;
+            }
+
+            var f = 1;
+        }
+
+        var sparqlQuery = this.query.renderQuery(params);
+
+        var thisInstance = this;
+
+        this.client.sendRequest(sparqlQuery, function(responseString) {
+            thisInstance.handleResponse(responseString, res, params);
+        }, function(responseError) {
+            thisInstance.handleError(responseError, res);
+        });
+
+    };
 
     route.getContext = function() {
         return {
