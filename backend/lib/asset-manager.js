65955e98a3bd58e1102e148fcc1995e4c5776b5b
karelklima <karelklima@gmail.com>
2014-09-03 00:40:53 +0200

    backend - asset manager komentare

--- backend/lib/asset-manager.js
+++ backend/lib/asset-manager.js
@@ -1,5 +1,5 @@
 /**
- * Created by Karel on 18. 4. 2014.
+ * AssetManager
  */
 
 var util = require('util');
@@ -9,16 +9,21 @@ var _ = require('underscore');
 
 var settings = require('./settings');
 
+/*
+  * Basic class for CSSPile and JSPile
+  * Serves as a container for collection of files
+  */
 function Pile()
 {
-    this.useRand = true;
-    this.rand = '';
-    if (settings.options.development)
-        this.rand = '?v=' + crypto.randomBytes(20).toString('hex').substring(0,5);
+    // whether or not include random ?v query string to prevent browser cache
+    this.useRand = settings.options.development;
+    this.rand = '?v=' + crypto.randomBytes(20).toString('hex').substring(0,5);
+    // list of files
     this.list = [];
     this.regex = /\.*/;
 }
 
+// ADD file to Pile
 Pile.prototype.addPath = function(module, url, file)
 {
     if (this.useRand)
@@ -33,16 +38,21 @@ Pile.prototype.addPath = function(module, url, file)
     });
 };
 
+/**
+ * Returns string of html tags of files in collection
+ */
 Pile.prototype.htmlTags = function() {
     // overwrite me
 };
 
+// Pile for CSS files
 function CSSPile() {
     Pile.call(this);
-    this.regex = /\.css$/;
+    this.regex = /\.css$/; // match CSS files
 }
 util.inherits(CSSPile, Pile);
 
+// Generate proper CSS html links
 CSSPile.prototype.htmlTags = function() {
     var output = "";
     for (var i = 0; i < this.list.length; i++)
@@ -52,12 +62,14 @@ CSSPile.prototype.htmlTags = function() {
     return output;
 };
 
+// Pile for JS files
 function JSPile() {
     Pile.call(this);
     this.regex = /\.js$/;
 }
 util.inherits(JSPile, Pile);
 
+// Generate proper JS html tags
 JSPile.prototype.htmlTags = function() {
     var output = "";
     for (var i = 0; i < this.list.length; i++)
@@ -67,41 +79,49 @@ JSPile.prototype.htmlTags = function() {
     return output;
 };
 
-
+/**
+ * AssetManager
+ *
+ */
 function AssetManager()
 {
     this.baseDir = settings.modulesDirectory;
     this.baseUrl = '/';
+    // requests application modules setup
     this.setup = settings.getModulesSetup();
+    // init both piles
     this.jsPile = new JSPile();
     this.cssPile = new CSSPile();
+    // look for assets
     this.findFiles();
 }
 
+// Gets module assets directory
 AssetManager.prototype.getModuleAssetsDir = function(module)
 {
     return this.baseDir + '/' + module + '/assets';
 };
-
+// Gets module assets base URL
 AssetManager.prototype.getModuleAssetsUrl = function(module)
 {
     return this.baseUrl + 'assets/' + module;
 };
-
+// Gets module's application assets directory
 AssetManager.prototype.getApplicationAssetDir = function(module, application)
 {
     return this.baseDir + '/' + module + '/apps/' + application;
 };
-
+// Gets module's application assets URL
 AssetManager.prototype.getApplicationAssetUrl = function(module, application)
 {
     return this.baseUrl + 'assets/' + module + '/' + application;
 };
-
+// Looks for CSS and JS files in modules' and applications' directories
 AssetManager.prototype.findFiles = function()
 {
     for (var module in this.setup)
     {
+        // also include subdirectories
         var files = glob.sync(this.getModuleAssetsDir(module) + '/**/*.*');
         this.fillPiles(files, module);
 
@@ -112,7 +132,7 @@ AssetManager.prototype.findFiles = function()
         }
     }
 };
-
+// Adds found files to proper piles
 AssetManager.prototype.fillPiles = function(files, module, application)
 {
     var path = this.getModuleAssetsDir(module);
@@ -129,10 +149,11 @@ AssetManager.prototype.fillPiles = function(files, module, application)
         var assetPath = files[i].substring(this.baseDir.length);
         var assetUrl = url + files[i].substring(path.length);
 
+        // Adds CSS files to CSSPile
         if (assetPath.match(this.cssPile.regex)) {
             this.cssPile.addPath(module, assetUrl, assetPath);
         }
-
+        // Adds JS files to JSPile
         if (assetPath.match(this.jsPile.regex)) {
             this.jsPile.addPath(module, assetUrl, assetPath);
         }
@@ -140,4 +161,4 @@ AssetManager.prototype.fillPiles = function(files, module, application)
 };
 
 var instance = new AssetManager();
-module.exports = instance;
+module.exports = instance; // returns singleton instance
