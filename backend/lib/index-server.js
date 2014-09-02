926c32894ad103d27b721279c120c2d4484b4648
karelklima <karelklima@gmail.com>
2014-09-02 18:22:30 +0200

    backend - index server, komentare

--- backend/lib/index-server.js
+++ backend/lib/index-server.js
@@ -7,19 +7,21 @@ var settings = require('./settings');
 var assetManager = require('./asset-manager');
 
 module.exports = function(req, res, next) {
+    // prepare application configuration
     var configuration = {
         application : {
             title: settings.options["title"],
             home: settings.options["home"],
             pageNotFound : settings.options["page-not-found"],
-            modules: settings.getModulesSetup()
+            modules: settings.getModulesSetup() // calculates modules configuration
         },
         user: {}
     };
+    // send response to client
     res.render(settings.frontendDirectory + '/index.html', {
         title: settings.options["title"],
-        configuration: JSON.stringify(configuration),
-        css: assetManager.cssPile.htmlTags(),
-        js: assetManager.jsPile.htmlTags()
+        configuration: JSON.stringify(configuration), // include application configuration
+        css: assetManager.cssPile.htmlTags(), // modules CSS files list
+        js: assetManager.jsPile.htmlTags() // modules JS files list
     });
 };
